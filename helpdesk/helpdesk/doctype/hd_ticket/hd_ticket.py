28ba0adb83129cbc898c0eb26811074d7ab3b1ce
amaan-ahmad <amaanahmad582001@gmail.com>
2026-01-27 11:48:46 +0000

    fix: correct email subject formatting in ticket replies
    - Updated the subject line in the `HDTicket` class to remove the colon after "Re" for improved clarity and consistency in email communications when replying to tickets.

--- helpdesk/helpdesk/doctype/hd_ticket/hd_ticket.py
+++ helpdesk/helpdesk/doctype/hd_ticket/hd_ticket.py
@@ -663,7 +663,7 @@ class HDTicket(Document):
         skip_email_workflow = self.skip_email_workflow()
         medium = "" if skip_email_workflow else "Email"
         if f"#{self.name}" not in self.subject:
-            subject = f"Re: [#{self.name}]: {self.subject}"
+            subject = f"Re [#{self.name}]: {self.subject}"
         else:
             subject = self.subject
         sender = frappe.session.user
